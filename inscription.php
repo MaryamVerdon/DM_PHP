ac4066da4c4c5a96cb8260593446791da69e3f1a
Maryam Verdon <maryamverdon@gmail.com>
2021-01-14 00:23:55 +0100

    modif form inscription

--- inscription.php
+++ inscription.php
@@ -121,7 +121,7 @@
 
                                  <div class="container">
                                      <div class="row">
-                                         <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                                         <div class="col-8 mx-auto">
                                              <div class="card card-signin my-5">
                                                  <div class="card-body">
                                                      <h5 class="card-title text-center">Inscription</h5>
